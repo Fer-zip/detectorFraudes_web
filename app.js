230d1b628f2997380d94f0ce9ed047effc25b5e9
Fer-zip <jeanpierefernandez80@gmail.com>
2025-05-26 17:59:51 -0500

    falta el proceso de subida

--- app.js
+++ app.js
@@ -20,6 +20,23 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
         btnDarkMode.innerHTML = svgSun;
     }
+
+
+    const elements = document.querySelectorAll(".scroll-element");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible"); // Esto permite que desaparezca al salir
+        }
+      });
+    }, {
+      threshold: 0.1,
+    });
+
+    elements.forEach(el => observer.observe(el));
 });
 
 const inputImage = document.querySelector('#image')
